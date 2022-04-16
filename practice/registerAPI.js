8675398433aba2e4e570e9e04cc6bc31c05de1b5
diasm <diasm2@gmail.com>
2022-04-16 23:05:45 +0700

    구조변경

--- practice/registerAPI.js
+++ practice/registerAPI.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const User = require("./models/mongodb/userSchema")
+const User = require("./mongodb/userSchema")
 const bcrypt = require("bcrypt")
 
 // 회원가입
