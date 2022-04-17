2a6e6ddf9c4b359a7d1dc02c3b0ad5d04bae9338
diasm <diasm2@gmail.com>
2022-04-17 12:48:44 +0700

    쓸데없는거 삭제 및 수정

--- app.js
+++ app.js
@@ -1,39 +1,21 @@
 const express = require("express")
 const app = express()
-const sequelize = require("./models/index")
-// const router = express.Router()
-// const auth = require("./auth")
-// const register = require("./registerAPI")
-// const userinf = require("./info")
+
 const morgan = require("morgan")
 const cors = require("cors")
-const api = require("./router/routers")
-// const sequealizlite= require("./sql")
-
-// const MongoUri= "mongodb+srv://diasm:83XZZ8LwO0rI95en@cluster0.mye6i.mongodb.net/cluster0?retryWrites=true&w=majority"
-
-
-// mongoose.connect(MongoUri, {
-//         useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// })
-
+// const api = require("./router/routers")
+const router = require("./router/postRouter")
 
 app.use(cors())
 app.use(morgan("dev"))
-app.use(express.urlencoded({ extended: false}) )
+app.use(express.urlencoded({ extended: false }))
 
-sequelize
-app.use("/api", api)
-
+app.use("/api", router)
+// app.use("/api", router)
+// app.use("/api", router)
 
 const PORT = 5500
 
 app.listen(PORT, () => {
-    console.log(`서버가 요청을 받을 준비가 됐어요 ${PORT}`)
+  console.log(`서버가 요청을 받을 준비가 됐어요 ${PORT}`)
 })
-
-
-
-
-
