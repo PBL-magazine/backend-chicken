88e1b0dea5856f4d1d75feb59b46d4b74e2673c4
diasm <diasm2@gmail.com>
2022-04-18 13:41:02 +0700

    [기능추가]쿠키세션 설정완료

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require("express")
+const cookieSession = require("cookie-session")
 const app = express()
 
 const morgan = require("morgan")
 const cors = require("cors")
 
+app.use(
+  cookieSession({
+    name: "blog",
+    keys: ["keys"],
+
+    maxAge: 24 * 60 * 60 * 1000, //24hours
+  })
+)
 app.use(express.json())
 app.use(cors())
 app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: false }))
 
 app.use("/api", require("./src/router/postRouter"))
-
 
 const PORT = 5500
 
