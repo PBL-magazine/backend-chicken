5e3c006672674f489b1de5de80f9bd0f28b37278
diasm <diasm2@gmail.com>
2022-04-18 23:00:27 +0700

    [기능추가]쿠키 + AUTH미들웨어 추가

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express")
 const cookieSession = require("cookie-session")
 const app = express()
-
+const jwt = require("jsonwebtoken")
 const morgan = require("morgan")
 const cors = require("cors")
 
 app.use(
   cookieSession({
     name: "blog",
-    keys: ["keys"],
+    keys: ["how"],
 
     maxAge: 24 * 60 * 60 * 1000, //24hours
   })
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cors())
 app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: false }))
-
+app.get("/auth", require("./src/auth/auth"))       // 인증처리 미들웨어
 app.use("/api", require("./src/router/routers"))
 
 const PORT = 5500
