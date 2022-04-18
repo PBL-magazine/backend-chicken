d726619543c0b79a56de0c074069990b6cf79260
diasm <diasm2@gmail.com>
2022-04-18 23:04:50 +0700

    [기능구현]CURD는 잘됨

--- src/controllers/postControllers.js
+++ src/controllers/postControllers.js
@@ -1,6 +1,7 @@
 const db = require("../models/index")
 
 const Posts = db.posts
+const Comments = db.comments
 const Users = db.users
 
 // 게시물 전체 조회	/api/posts	GET
@@ -53,9 +54,21 @@ const readPost = async (req, res) => {
   console.log(post)
 }
 
-// // 특정 게시물 수정	/api/posts/:post_id	PATCH
+// // 특정 게시물 수정
 // router.patch('/posts/:post_id')
 const updatePost = async (req, res) => {
+  let id = req.params.post_id
+  console.log(id)
+
+  // const info = ({ title, content } = req.body)
+
+  const post = await Posts.update(req.body, { where: { post_id: id } })
+  res.status(200).send(post)
+}
+
+// // 특정 게시물 삭제	/api/posts/:post_id	DELETE
+// router.delete('/posts/:post_id')
+const deletePost = async (req, res) => {
   const post_id = req.params.post_id
 
   await Posts.update(
