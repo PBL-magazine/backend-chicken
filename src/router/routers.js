de4251c316f483f918086475c0a7f630516406da
diasm <diasm2@gmail.com>
2022-04-19 13:34:14 +0700

    [삭제예정]라우터 -> 컨트롤러로 변경

--- src/router/routers.js
+++ src/router/routers.js
@@ -1,16 +1,15 @@
 const router = require("express").Router()
-const authMiddleware = require("../auth/auth")
-const postController = require("../controllers/postControllers")
+const auth = require("../auth/auth")
+const postController = require("../controllers/postController")
 const userController = require("../controllers/userController")
 const commentController = require("../controllers/commentController")
-const auth = require("../auth/auth")
 
 // posting
-router.post("/posts", auth, postController.createPost) // 게시물 등록
-router.get("/posts", postController.fetchList) // 게시물 전체 보여주기
-router.get("/posts/:post_id", postController.readPost) // 게시물 하나 읽기
-router.put("/posts/:post_id", auth, postController.updatePost) // 게시물 하나 업데이트 하기
-router.delete("/posts/:post_id", auth, postController.deletePost) // 게시물 하나 지우기
+// router.post("/posts", auth, postController.createPost) // 게시물 등록
+// router.get("/posts", postController.fetchList) // 게시물 전체 보여주기
+// router.get("/posts/:post_id", postController.readPost) // 게시물 하나 읽기
+// router.put("/posts/:post_id", auth, postController.updatePost) // 게시물 하나 업데이트 하기
+// router.delete("/posts/:post_id", auth, postController.deletePost) // 게시물 하나 지우기
 
 // like
 router.put("/posts/:post_id/like",auth) // 특정 게시물 좋아요 상태 변경
