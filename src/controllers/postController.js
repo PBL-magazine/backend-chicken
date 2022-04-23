34e7148a503e8f4acd68b972808d7e042debfab9
diasm <diasm2@gmail.com>
2022-04-23 12:42:17 +0700

    [기능추가] 이미지 업로드 추가

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,56 +1,53 @@
 const router = require("express").Router()
+const { upload } = require("../../utils/upload")
 const auth = require("../auth/auth")
 const POST_SERVICE = require("../service/postService")
-const Message = require("../../utils/msg")
-const multer = require("multer")
-const path = require("path")
 
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "uploads/")
-    },
-    filename: function (req, file, cb) {
-      cb(null, new Date().valueOf() + path.extname(file.originalname))
-    },
-  }),
-})
+const Message = require("../../utils/msg")
+
 /*############# controller ###################
 컨트롤러는 데이터를 라우터 + 받아오는 
 #############################################*/
+
+
+
+
 
 // 전체 게시물 조회
 router.get("/posts", async (req, res) => {
   try {
     const result = await POST_SERVICE.getAllPosts()
 
-    res.status(200).send(Message.success(result, "rows"))
+    return res.status(200).send(Message.success(result, "rows"))
   } catch (err) {
-    res.status(500).send(Message.Err500)
+
+    return res.status(500).send(Message.Err500)
   }
 })
+
 
 // 게시물 생성
 router.post("/posts", auth, upload.single("image"), async (req, res) => {
   try {
     const { user_id } = res.locals.info
+    const { content } = req.body
+    const image_url = "/uploads/" + req.file.filename
+
 
     if (!user_id) {
       res.status(401).send(Message.Err401)
     }
-
-    const { content } = req.body
-    const image_url = "/uploads/" + req.file.filename
 
     if (!content && !image_url) {
       res.status(400).send(Message.Err400)
     }
 
     POST_SERVICE.createPost({ content, image_url, user_id })
-    res.status(200).send(Message.success())
+
+    return res.status(200).send(Message.success())
   } catch (err) {
     console.log(err)
-    res.status(500).send(Message.Err500)
+    return res.status(500).send(Message.Err500)
   }
 })
 
@@ -60,9 +57,9 @@ router.get("/posts/:post_id", async (req, res) => {
     const post_id = req.params.post_id
     const result = await POST_SERVICE.getPost(post_id)
 
-    res.status(200).send(Message.success(result, "row"))
+    return res.status(200).send(Message.success(result, "row"))
   } catch (err) {
-    res.status(500).send(Message.Err500)
+    return res.status(500).send(Message.Err500)
   }
 })
 
@@ -73,10 +70,10 @@ router.patch("/posts/:post_id", auth, async (req, res) => {
 
     await POST_SERVICE.updatePost(req.body, post_id)
 
-    res.status(200).send({ ok: true })
+    return res.status(200).send({ ok: true })
   } catch (err) {
     console.log(err)
-    res.status(500).send(Message.Err500)
+    return res.status(500).send(Message.Err500)
   }
 })
 
@@ -86,12 +83,11 @@ router.delete("/posts/:post_id", auth, async (req, res) => {
     //관련되 댓글을 다 삭제 할 것인가? 생각해봐야함
     const post_id = req.params.post_id
 
-    POST_SERVICE.deletePost(post_id)
+    await POST_SERVICE.deletePost(post_id)
 
-    res.status(200).send({ ok: true })
+    return res.status(200).send({ ok: true })
   } catch (err) {
-    console.log(err)
-    res.status(500).send(Message.Err500)
+    return res.status(500).send(Message.Err500)
   }
 })
 
