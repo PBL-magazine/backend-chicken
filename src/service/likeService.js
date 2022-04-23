d17ba22e169ff82a5496866008d7953b6ad48944
diasm <diasm2@gmail.com>
2022-04-23 12:43:57 +0700

    [구조변경]repo 분리

--- src/service/likeService.js
+++ src/service/likeService.js
@@ -1,62 +1,23 @@
-const { Op } = require("sequelize")
-const { post } = require("../controllers/userController")
-const db = require("../models/index")
+const likeRepo = require("../Repository/likeRepo")
 
-const Likes = db.likes
 /*############# Service logic ###################
 1. bussiness logic 이라고 하다
 2. Repository( data Access 기능)
 Client -> Service -> Repository -> DB
 #############################################*/
 
-class Repository {
-  saveLike = async (post_id, user_id) => {
-    await Likes.create({ post_id, user_id })
-  }
-
-  removeLike = async (post_id, user_id) => {
-    await Likes.destroy({
-      where: {
-        [Op.and]: [{ post_id: post_id }, { user_id: user_id }],
-      },
-    })
-  }
-
-  checkLike = async (post_id, user_id) => {
-    return await Likes.findOne({
-      where: {
-        post_id,
-        user_id,
-      },
-    })
-  }
-}
-
-const rep = new Repository()
-
 const likeService = {
-  saveCounter: async (post_id, user_id) => {
-    const result = await rep.saveLike(post_id, user_id)
-  },
 
-  fetchLikesCount: async (post_id) => {
-    return await Likes.count({
-      where: { post_id: post_id },
-    })
-  },
+  toggleLike: async (post_id, user_id) => {
+    const isLike = await likeRepo.checkLike(post_id, user_id)
 
-
-  test: async (post_id, user_id) => {
-    const result = await rep.checkLike(post_id, user_id)
-
-    if (result) {
-      await rep.removeLike(post_id, user_id)
+    if (isLike) {
+      await likeRepo.removeLike(post_id, user_id)
     } else {
-      await rep.saveLike(post_id, user_id)
+      await likeRepo.saveLike(post_id, user_id)
     }
 
-    console.log(result)
-    return result
+    return isLike
   },
 }
 
--- src/service/postService.js
+++ src/service/postService.js
@@ -1,86 +1,27 @@
-const db = require("../models/index")
-const LIKE_SERVICE = require("../service/likeService")
-
-const Posts = db.posts
-const Users = db.users
-const Likes = db.likes
-
-/*############# Service logic ###################
-1. bussiness logic 이라고 하다
-2. Repository( data Access 기능)
-Client -> Service -> Repository -> DB
-#############################################*/
-
-class Repository {
-  Create = async (data) => {
-    const { content, image_url, user_id } = data
-    return await Posts.create({ content, image_url, user_id })
-  }
-
-  ReadbyId = async (post_id) => {
-    return await Posts.findOne({
-      // raw: true,
-      // nest: true,
-      include: [
-        {
-          model: Users,
-          as: "user",
-          attributes: ["user_id", "email", "nickname"],
-        },
-        {
-          model: Likes,
-          as: "likes",
-          attributes: ["user_id"],
-        },
-      ],
-      where: { post_id },
-    })
-  }
-
-  ReadAll = async () => {
-    return await Posts.findAll({
-      // raw: true,
-      // nest: true,
-      order : [['createdAt', 'DESC']],
-      include: [
-        {
-          model: Users,
-          as: "user",
-          attributes: ["user_id", "email", "nickname"],
-        },
-        {
-          model: Likes,
-          as: "likes",
-          attributes: ["user_id"],
-        },
-      ],
-    })
-  }
-
-  Update = async (content, post_id) => {
-    return await Posts.update(content, { where: { post_id: post_id } })
-  }
-
-  Delete = async (post_id) => {
-    return await Posts.destroy({
-      where: { post_id },
-    })
-  }
-}
-
-const repo = new Repository()
+"use strict"
+// const  postRepo  = new (require("./repository"))()
+const postRepo = require("../Repository/postRepo")
 
 const postService = {
-  createPost: async (data) => repo.Create(data),
+  createPost: async (data) => {
+   return await postRepo.Create(data)
+  },
 
-  getAllPosts: async () => await repo.ReadAll(),
+  getAllPosts: async () => {
+   return await postRepo.ReadAll()
+  },
 
-  getPost: async (post_id) => await repo.ReadbyId(post_id),
+  getPost: async (post_id) => {
+   return await postRepo.ReadbyId(post_id)
+  },
 
-  updatePost: async (content, post_id) =>
-    await repo.Update(content, post_id),
+  updatePost: async (content, post_id) => {
+   return await postRepo.Update(content, post_id)
+  },
 
-  deletePost: async (post_id) => await repo.Delete(post_id),
+  deletePost: async (post_id) => {
+   return await postRepo.Delete(post_id)
+  },
 }
 
 module.exports = postService
