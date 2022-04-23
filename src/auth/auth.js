ed16983654c8390f0f4a9e929b9e4feec2ab9d59
diasm <diasm2@gmail.com>
2022-04-23 12:41:11 +0700

    [삭제]필요없는 내용삭제

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,28 +1,34 @@
 const jwt = require("jsonwebtoken")
 const Message = require("../../utils/msg")
-const jm = require("../models/index")
 const dotenv = require("dotenv")
-
-const Users = jm.users
+const findEmail = require("../repository/userRepo")
 
 dotenv.config({ path: "../../utils/config.env" })
+const getAppCookies = (req) => {
+  const rawCookies = req.headers.cookie.split("; ")
+
+  const parsedCookies = {}
+  rawCookies.forEach((rawCookie) => {
+    const parsedCookie = rawCookie.split("=")
+    parsedCookies[parsedCookie[0]] = parsedCookie[1]
+  })
+  return parsedCookies
+}
 
 const authMiddleware = async (req, res, next) => {
-  // cookie-session 라이브러리를 이용해서 발생된 토큰을 클라이언트에 주입
   try {
-    const bearerHeader = req.session.token
+    // const bearerHeader = req.session.token
+    // console.log(req.headers, + "cookie있어?")
 
-    if (typeof bearerHeader !== "undefined") {
-      const info = jwt.decode(bearerHeader, process.env.JWT_SECRECT)
+    // console.log(token)
 
-      const data = await Users.findOne({
-        where: {
-          email: info.email,
-        },
-        attributes: ["user_id", "nickname", "email", "role"],
-      })
+    if (typeof req.headers.cookie !== "undefined") {
+      const { token }= getAppCookies(req)
+      const { email } = jwt.decode(token, process.env.JWT_SECRECT)
 
-      res.locals.info = data.dataValues
+      const result = await findEmail.searchByEmail(email)
+
+      res.locals.info = result.dataValues
       next()
     } else {
       res.status(401).send(Message.errMsg.Err401)
